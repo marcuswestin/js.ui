ff88b541af564d0d2f30fce9f81974f54deafc60
Marcus Westin <marcus.westin@gmail.com>
2021-02-08 09:45:33 -0500

    Start bringing native engine up to speed

--- native.ts
+++ native.ts
@@ -1,33 +1,47 @@
-import React, { View as ReactNativeView, Text as ReactNativeText } from 'react-native'
-import { NativeViewProperties, NativeViewStyles, NativeTextProps, NativeTextStyles, View } from "./src/js.ui-types"
-import { setViewMakers } from "./src/js.ui-core"
+import { View as ReactNativeView, Text as ReactNativeText } from 'react-native'
+import { View, NativeViewProperties, NativeViewStyles, NativeTextProps, NativeTextStyles, NativeViewArg, TextValue } from "./src/js.ui-types"
+import { makeView, setViewMakers } from "./src/js.ui-core"
+import React from 'react'
 
-import { Row, Col, TextView, FlexFix, Flex, Padding, Margin } from "./universal"
-export { Row, Col, TextView, FlexFix, Flex, Padding, Margin }
+import { FlexFix, Flex, Padding, Margin } from "./universal"
+export { FlexFix, Flex, Padding, Margin }
 
 // View maker functions
 ///////////////////////
+
+function makeTextView(properties: NativeTextProps, text: string) {
+    return React.createElement(ReactNativeText, properties, text)
+}
 
 setViewMakers({
     engine: 'Native',
     makeView(properties: NativeViewProperties, children: View[]) {
         return React.createElement(ReactNativeView, properties, children)
     },
-    makeTextView(properties: NativeTextProps, text: string) {
-        return React.createElement(ReactNativeText, properties, text)
-    },
+    makeTextView,
 })
 
 // Native-specific
 ///////////////
 
-export function NativeTextView(text: string, properties?: NativeTextProps, styles?: NativeTextStyles): View {
-    properties.style = Object.assign(properties.style, styles)
-    return React.createElement('span', properties, text)
+Row.styles = Style({ display:'flex', flexDirection: 'row' })
+export function Row(...args: NativeViewArg[]): View {
+    return makeView(Row.styles, ...args)
+}
+
+Col.styles = Style({ display:'flex', flexDirection: 'column' })
+export function Col(...args: NativeViewArg[]): View {
+    return makeView(Col.styles, ...args)
 }
 
 export function Style(styles: NativeViewStyles): NativeViewProperties {
     return { style:styles }
 }
 
-export let NativeElement = React.createElement
+export function Text(text: TextValue, properties?: NativeTextProps, styles?: NativeTextStyles): View {
+    if (!properties) { properties = {} }
+    properties.style = Object.assign(properties.style || {}, styles)
+    return makeTextView(properties, text.toString())
+}
+
+// export let NativeElement = React.createElement
